493f7604f4ce15acc95a7540e4a9096c2f549567
Mark Thanadabouth <mark28ten@gmail.com>
2022-03-03 21:17:43 -0800

    Updated authRoutes.js
    Took out console logs.

--- src/auth/authRoutes.js
+++ src/auth/authRoutes.js
@@ -19,7 +19,6 @@ async function handleCreate (req, res, next) {
     const output = {
       user: userRecord,
     };
-    // console.log('User Create Test: ', output);
     res.status(201).json(output);
   } catch (e) {
     next(e.message)
@@ -30,7 +29,6 @@ function handleSignin (req, res, next) {
   const user = {
     user: req.user,
   };
-  // console.log('User Sign-in Test: ', user);
   res.status(200).json(user);
 };
 
@@ -38,7 +36,6 @@ async function handleGetAll (req, res, next) {
   const userRecords = await users.findAll({});
   const list = userRecords.map(user =>
     `User/Username: ${user.username}, User Role: ${user.role}, ID: ${user.userID}`);
-  // console.log('Admin is able to get list test: ', list);
   res.status(200).json(list);
 };
 
@@ -47,15 +44,12 @@ async function handleUpdate (req, res, next) {
   let obj = req.body;
   let updatedRecord = await users.findOne({ where: { userID } })
     .then(record => record.update(obj));
-  // console.log('Admin is able to update Test: ', updatedRecord);
   res.status(200).json(updatedRecord);
 };
 
 async function handleDelete (req, res, next) {
   const userID = req.params.id;
-  console.log('*****', userID)
   const deletedUser = await users.destroy({where: { userID }});
-  // console.log('Admin is able to delete user Test: ', deletedUser);
   res.status(200).json(`User Deleted ${deletedUser}`);
 };
 
