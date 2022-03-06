87b985c74804cccf2fbd96506229ac02bcfcb9a8
Charlie <boy_916@hotmail.com>
2022-03-06 13:49:42 -0800

    added try catch and update

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -8,16 +8,24 @@ router.post('/comment-creation', createComment)
 router.get('/comment-info', getCommentData)
 
 async function getCommentData (req, res) {
-  let allComments = await comments.findAll()
-  console.log('allComments', allComments)
-  res.status(200).json(allComments)
+  try {
+    let allComments = await comments.findAll()
+    console.log('allComments', allComments)
+    res.status(200).json(allComments)
+  } catch (e) {
+    res.status(500).send('Get Comment Error')
+  }
 }
 
 async function createComment(req, res) {
-  let commentData = req.body
-  console.log('commentData:', commentData);
-  let postComment = await comments.create(commentData)
-  res.status(200).json(postComment)
+  try {
+    let commentData = req.body
+    console.log('commentData:', commentData);
+    let postComment = await comments.create(commentData)
+    res.status(200).json(postComment)
+  } catch (e) {
+    res.status(500).send('Create Comment Error')
+  }
 }
 
 module.exports = router
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,14 +8,22 @@ router.post('/user-creation', createUser)
 router.get('/user-info', getUserData)
 
 async function getUserData (req, res) {
-  let allUsers = await users.findAll()
-  res.status(200).json(allUsers)
+  try {
+    let allUsers = await users.findAll()
+    res.status(200).json(allUsers)
+  } catch (e) {
+    res.status(500).send('Get User Error')
+  }
 }
 
 async function createUser(req, res) {
-  let userData = req.body
-  let postUser = await users.create(userData)
-  res.status(200).json(postUser)
+  try {
+    let userData = req.body
+    let postUser = await users.create(userData)
+    res.status(200).json(postUser)
+  } catch (e) {
+    res.status(500).send('Create User Error')
+  }
 }
 
 module.exports = router
