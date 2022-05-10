1cfc9bbfa4573e9161d235797353bf4660a930fb
Charlie Fadness <82005916+fadnesscharlie@users.noreply.github.com>
2022-05-10 00:29:01 -0700

    Took out custom cors

--- src/server.js
+++ src/server.js
@@ -22,16 +22,19 @@ const app = express();
 
 // Use the middleware
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use(cors({
-  origin: *,
-  methods: ["GET", "POST", "PUT"]
-}));
+// app.use(cors({
+//   origin: *,
+//   methods: ["GET", "POST", "PUT"]
+// }));
+
+
+app.use(cors())
 app.use(express.json());
 
 // Routes
