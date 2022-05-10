749136e1ed5dd70fa5f8e7f5d31f12974605ecf2
Charlie Fadness <fadness.charlie@outlook.com>
2022-05-10 00:32:57 -0700

    corsTest

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 // 3rd Party
-const express = require('express');
+const express = require("express");
 // const cors = require('cors');
 
 // Route Imports
-const authRoutes = require('./auth/authRoutes')
+const authRoutes = require("./auth/authRoutes");
 
 // Routes
-const userRoutes = require('./routes/user')
-const petRoutes = require('./routes/pet')
-const commentRoutes = require('./routes/comment')
-const markerRoutes = require('./routes/marker')
+const userRoutes = require("./routes/user");
+const petRoutes = require("./routes/pet");
+const commentRoutes = require("./routes/comment");
+const markerRoutes = require("./routes/marker");
 
 // Error Handlers
-const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
+const errorHandler = require("./error-handlers/500.js");
+const notFound = require("./error-handlers/404.js");
 
 // Prepare the express app
 const app = express();
@@ -27,41 +27,50 @@ const app = express();
 //   next();
 // });
 
-app.use(cors({
-  origin: "https://lost-pet-finder-floof-finders.herokuapp.com"
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(cors())
+
+// app.use(cors({
+//   origin: '',
+//   methods: ["GET", "POST", "PUT"]
+// }));
 
 app.use(express.json());
 
 // Routes
 app.use(authRoutes);
 // modularize later
-app.get('/', (req, res) => { 
-  res.status(200).send('You have reached the Wizard!');
+app.get("/", (req, res) => {
+  res.status(200).send("You have reached the Wizard!");
 });
-app.get('/bad', (req, res) => {
-  res.status(404).send({notFound})
-})
+app.get("/bad", (req, res) => {
+  res.status(404).send({ notFound });
+});
 
 // Routes
-app.use(userRoutes)
-app.use(petRoutes)
-app.use(commentRoutes)
-app.use(markerRoutes)
+app.use(userRoutes);
+app.use(petRoutes);
+app.use(commentRoutes);
+app.use(markerRoutes);
 
 // Catchalls
-app.use('*', notFound);
+app.use("*", notFound);
 app.use(errorHandler);
 
 // Start
 module.exports = {
   server: app,
-  start: port => {
-    if(!port) {
-      throw new Error('Not the Mama!');
+  start: (port) => {
+    if (!port) {
+      throw new Error("Not the Mama!");
     }
     app.listen(port, () => {
       console.log(`Server up on ${port}`);
     });
-  }
-}
+  },
+};
